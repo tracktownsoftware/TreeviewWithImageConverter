f686e0b958c07d73bf0b54c1777580a0a5c34cb1
Chris <chris@tracktownsoftware.com>
2021-05-13 15:38:09 -0700

    Changed build action to resource on image files

--- TreeviewWithImageConverter/HeaderToImageConverter.cs
+++ TreeviewWithImageConverter/HeaderToImageConverter.cs
@@ -6,33 +6,35 @@ using System.Windows.Media.Imaging;
 
 namespace TreeviewWithImageConverter
 {
-    [ValueConversion(typeof(string), typeof(bool))]
     public class HeaderToImageConverter : IValueConverter
     {
         public static HeaderToImageConverter Instance = new HeaderToImageConverter();
 
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
+            // If image files Build Action is None and copy always then use this approach to load images
+            //      string filePath = System.IO.Path.GetDirectoryName(System.AppDomain.CurrentDomain.BaseDirectory) + "\\images\\";
+            //      uri = new Uri(filePath + "FolderOpened-32.png", UriKind.RelativeOrAbsolute);
+
+            // Code below uses image files Build Action is Resource and copy always
             if (value is TreeViewModel)
             {
                 TreeViewModel tvm = (TreeViewModel)value;
-
-                string filePath = System.IO.Path.GetDirectoryName(System.AppDomain.CurrentDomain.BaseDirectory) + "\\images\\";
 
                 Uri uri = null;
                 switch (tvm.TreeItemType)
                 {
                     case TreeItemType.Folder:
                         if (tvm.IsNodeExpanded)
-                            uri = new Uri(filePath + "FolderOpened-32.png", UriKind.RelativeOrAbsolute);
+                            uri = new Uri("/images/FolderOpened-32.png", UriKind.Relative);
                         else
-                            uri = new Uri(filePath + "FolderClosed-32.png", UriKind.RelativeOrAbsolute);
+                            uri = new Uri("/images/FolderClosed-32.png", UriKind.Relative);
                         break;
                     case TreeItemType.EngineCode:
-                        uri = new Uri(filePath + "EngineCode.png", UriKind.RelativeOrAbsolute);
+                        uri = new Uri("/images/EngineCode.png", UriKind.Relative);
                         break;
                     case TreeItemType.WindowsCode:
-                        uri = new Uri(filePath + "WindowsCode.png", UriKind.RelativeOrAbsolute);
+                        uri = new Uri("/images/WindowsCode.png", UriKind.Relative);
                         break;
                 }
                 BitmapImage source = new BitmapImage(uri);
